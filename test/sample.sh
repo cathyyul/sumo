f29fe0e2eca807a4e05c40a6886641ae402aee6e
Yu-Ting Yu <yutingyu@cs.ucla.edu>
2013-12-06 14:22:25 -0800

    update sample.sh paths

--- test/sample.sh
+++ test/sample.sh
@@ -3,15 +3,15 @@ TRIPTIME=100
 PERIOD=1
 ROUTETIME=800
 GRIDNUM=5
-SUMOPATH=/home/mars/sumo
-NAME=test
+SUMOPATH=$HOME/sumo
+NAME=grid
 
 python $SUMOPATH/tools/trip/randomTrips.py -n $NAME.net.xml -e $TRIPTIME -p $PERIOD -o $NAME.trip.xml
 
-duarouter -t $NAME.trip.xml -n $NAME.net.xml -e $ROUTETIME -o $NAME.rou.xml
+$SUMOPATH/bin/duarouter -t $NAME.trip.xml -n $NAME.net.xml -e $ROUTETIME -o $NAME.rou.xml
 
-sumo -c $NAME.sumocfg --fcd-output $SCENARIO.fcd.xml
+$SUMOPATH/bin/sumo -c $NAME.sumocfg --fcd-output $SCENARIO.fcd.xml
 
-sh fcd-qualnet.sh $SUMOPATH $SCENARIO.fcd.xml $SCENARIO.mob
+sh $SUMOPATH/tools/bin/fcd-qualnet.sh $SUMOPATH $SCENARIO.fcd.xml $SCENARIO.mob
 
 sh sumo-corner.sh $GRIDNUM $NAME.nod.xml $NAME.edg.xml $NAME.nodes $NAME.links
